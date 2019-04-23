ebc3c49783294674c36b2b4aebf059fc093233bf
arashrestart <arashguest1@gmail.com>
2019-04-23 06:12:14 -0400

    Updates

---
+++ precache-manifest.64698379780465abad1b3941092f058e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890efd88998d7695412e7675cbb41da0",
+    "url": "/ArabicTv/index.html"
+  },
+  {
+    "revision": "9162a1de60a0ed51bea2",
+    "url": "/ArabicTv/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e22fc23efc526cd10687",
+    "url": "/ArabicTv/static/css/main.ede7a866.chunk.css"
+  },
+  {
+    "revision": "9162a1de60a0ed51bea2",
+    "url": "/ArabicTv/static/js/2.ec080f1f.chunk.js"
+  },
+  {
+    "revision": "e22fc23efc526cd10687",
+    "url": "/ArabicTv/static/js/main.c3a32b1a.chunk.js"
+  },
+  {
+    "revision": "0403335bc0f7e96f5453",
+    "url": "/ArabicTv/static/js/runtime~main.dcd4a74c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d659ba549e571cef559b1f665afc4bf.js"
+  "/ArabicTv/precache-manifest.64698379780465abad1b3941092f058e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ArabicTv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
