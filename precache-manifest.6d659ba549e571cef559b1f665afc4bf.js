d5c0946c1342f185c0bd9293c927c06dc68cc243
arashrestart <arashguest1@gmail.com>
2019-04-23 06:11:02 -0400

    Updates

---
+++ precache-manifest.6d659ba549e571cef559b1f665afc4bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da836dd3fc3ecbc32f27c1f06c4478b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9162a1de60a0ed51bea2",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "919e6d79815a4c3635d1",
+    "url": "/static/css/main.ede7a866.chunk.css"
+  },
+  {
+    "revision": "9162a1de60a0ed51bea2",
+    "url": "/static/js/2.ec080f1f.chunk.js"
+  },
+  {
+    "revision": "919e6d79815a4c3635d1",
+    "url": "/static/js/main.e9248233.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
